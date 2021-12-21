a6bb35c7061c81a24b9f0165d4684054a9125e87
Matthias Huerbe <huerbe@vrvis.at>
2021-12-21 21:45:06 +0100

    moved emotion box spawning into update

--- Assets/CognitiveXR/Scripts/EmotionManager.cs
+++ Assets/CognitiveXR/Scripts/EmotionManager.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
@@ -11,28 +12,35 @@ public class EmotionManager : MonoBehaviour
 
     private readonly List<EmotionBox> spawnedBoxes = new List<EmotionBox>();
 
-    
+    private ConcurrentQueue<EmotionBox.EmotionInfo> receivedEmotionDetectedEvents = new ConcurrentQueue<EmotionBox.EmotionInfo>();
+
     private void Awake()
     {
         Debug.Assert(EmotionBoxPrefab != null);
         emotionDetectionComponent.OnEmotionDetected += OnEmotionDetected;
     }
 
+    private void Update()
+    {
+        while (receivedEmotionDetectedEvents.TryDequeue(out EmotionBox.EmotionInfo info))
+        {
+            CleanupOldEmotionBoxes(info.frameId);
+
+            GameObject emotionBoxGO = Instantiate(EmotionBoxPrefab, info.cameraPose.position, Quaternion.identity);// info.cameraPose.rotation );
+
+            EmotionBox emotionBox = emotionBoxGO.GetComponent<EmotionBox>();
+
+            emotionBox.Init(info);
+        
+            spawnedBoxes.Add(emotionBox);
+        }
+    }
 
     private void OnEmotionDetected(EmotionBox.EmotionInfo info)
     {
-        CleanupOldEmotionBoxes(info.frameId);
-
-        GameObject emotionBoxGO = Instantiate(EmotionBoxPrefab, info.cameraPose.position, Quaternion.identity);// info.cameraPose.rotation );
-
-        EmotionBox emotionBox = emotionBoxGO.GetComponent<EmotionBox>();
-
-        emotionBox.Init(info);
-        
-        spawnedBoxes.Add(emotionBox);
+        receivedEmotionDetectedEvents.Enqueue(info);
     }
 
-    
     private void CleanupOldEmotionBoxes(uint newFrameId)
     {
         foreach (EmotionBox spawnedBox in spawnedBoxes)
